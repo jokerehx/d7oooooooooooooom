4792fc953fbba7dca96687a96e4a28165b13c0fd
jokerehx <41896809+jokerehx@users.noreply.github.com>
2018-11-09 15:03:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`سسس`,"http://twitch.tv/S-F")
+client.user.setGame(`- على كثر الغياب قلبي مانسّى ঌ .`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
